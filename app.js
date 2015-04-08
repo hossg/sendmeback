ccd26722e319efb96f67acde621cd0b31a05afcd
hossg <hossein.ghodse@gmail.com>
2015-04-08 21:17:50 +0100

    Fixed issue #2 - Allow pre-defined pages/URLs to be returned - but with a specific HTTP status code and headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 
+var path = require("path")
 var url = require('url')
 var fs = require('fs')
 
@@ -10,28 +11,47 @@ var app = express()
 app.set('view engine','jade')
 app.use(express.static('public'))
 
-app.get('/sendmeback/*',function (req, res) {
-  paths=url.parse(req.url).pathname.split('/')
-  code=200
-  for(n=0;n<paths.length;n++){
-    rc=parseInt(paths[n])
-    if(!isNaN(rc)){ //look for the first thing that looks like a number and use that as the response code
-      code=rc
-      break;
+
+function getCode(req){
+    var paths=url.parse(req.url).pathname.split('/')
+    var code=200
+    for(n=0;n<paths.length;n++){
+        rc=parseInt(paths[n])
+        if(!isNaN(rc)){ //look for the first thing that looks like a number and use that as the response code
+            code=rc
+            break;
+        }
     }
-  }
-  res.set(req.query)
-  res.writeHead(code)
-  msg=code + ':' + req.url
-  res.end(msg)
-  console.log(msg)
-  console.log(req.query)
+    return code;
+}
+
+function sendBack(req, res, content) {
+    var code = getCode(req)
+    res.set(req.query)
+    res.writeHead(code)
+    res.end(content)
+    return code
+}
+
+app.get('/sendmeback/static/*',function (req, res) {
+    var filename = "static/" + path.basename(url.parse(req.url).pathname);
+    var page = fs.readFileSync(filename,{'encoding':'utf8'})
+    var code=sendBack(req,res, page)
+    console.log("Sending static file: " + code + ' ' + JSON.stringify(req.query) + ' ' + filename)
+})
+
+app.get('/sendmeback/*',function (req, res) {
+    var code = getCode(req)
+    var msg=(code + ':'+ req.url)
+    var code = sendBack(req,res, msg)
+    console.log("Sending: " + code + ' ' + JSON.stringify(req.query) + ' ' + req.url)
 })
 
 app.get('/', function (req, res) {
     var readme = fs.readFileSync('README.md',{'encoding':'utf8'})
     var readmeHTML = md.render(readme);
     res.render('index',{title:'SendMeBack',content:readmeHTML})
+    console.log('Sending index/readme page')
 })
 
 var port = process.env.PORT || 8080;  //let heroku define the port
@@ -41,6 +61,6 @@ var server = app.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('SendMeBack app listening at http://%s:%s', host, port)
 
 })
